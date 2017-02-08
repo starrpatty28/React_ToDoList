6bde04fcfb0682b87c1bc1d6cc550be568f15eae
starrpatty28 <starrpatty28@gmail.com>
2017-02-07 16:18:18 -0800

    add an components file and start coding in app.js file

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default App extends React.Component {  //this is what were calling our component
+  render () {
+    return (
+        <div>
+            <h1>React ToDos App</h1>
+        </div>
+    );
+  }
+}
