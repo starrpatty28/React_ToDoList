913af1f42201ffd29eb157505abdf99111cb7314
sashajustice <sabrinnasreddin@gmail.com>
2017-02-08 17:37:15 -0800

    create mock todos, imported into same page second paragraph,(added divs, todoslist

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import TodosList from './todos-list';
+
+const todos = [  //creating mock to dos
+  {
+      task: 'make React tutorial',
+      isCompleted: false
+},
+{
+      task: 'eat dinner',
+      isCompleted: true
+
+}
+];
 
 export default class App extends React.Component {  //this is what were calling our component
   render () {
     return (   //keeps code organzed, putting divs in there
         <div>
             <h1>React ToDos App</h1>
+            <TodosList/>
         </div>
     );
   }
