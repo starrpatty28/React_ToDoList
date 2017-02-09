9f30ae8b0d504c4b8ad9e29661d5b0cadfb18078
Brian <bboncy@gmail.com>
2017-02-09 14:27:53 -0800

    add spread synthax

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import React from 'react';
 import TodosListHeader from './todos-list-header';
 
 export default class TodosList extends React.Component {  //this is what were calling our component
     renderItems() {
-        return
+        return _.map(this.props.todos, (todo, index) => <TodosListenItem key={index // map creates a new array out of what we have. Allows us to customize how we want without mutating it.
+        } {...todo} />);  ///the triple dot is the spread syntax in es6. 
     }
 
   render () {
