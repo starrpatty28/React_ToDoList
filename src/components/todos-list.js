9ddfef59632a4c847d1c784fe3dd2add8ceb8f79
Brian <bboncy@gmail.com>
2017-02-09 14:31:48 -0800

    add spread syntax to todo-list.js

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -5,7 +5,7 @@ import TodosListHeader from './todos-list-header';
 export default class TodosList extends React.Component {  //this is what were calling our component
     renderItems() {
         return _.map(this.props.todos, (todo, index) => <TodosListenItem key={index // map creates a new array out of what we have. Allows us to customize how we want without mutating it.
-        } {...todo} />);  ///the triple dot is the spread syntax in es6. 
+        } {...todo} />);  ///the triple dot is the spread syntax in es6.
     }
 
   render () {
