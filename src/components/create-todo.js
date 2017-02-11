83e8994b6c81ea31a17c927c836d6ab313067e8e
starrpatty28 <starrpatty28@gmail.com>
2017-02-10 17:07:57 -0800

    add the ref identifier to output and add an event handeler to prevent page from automatic refresh when we hit create button

--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -3,10 +3,17 @@ import React from 'react';
 export default class TodosList extends React.Component {  //this is what were calling our component
   render () {
     return (   //keeps code organzed, putting divs in there
-      <form>
-        <input type="text" placeholder="What do I need to do?" />
+      <form onSubmit={this.handleCreate.bind(this)}>   {/*this allows what we put n text input box to actually become a task*/}
+        <input type="text" placeholder="What do I need to do?" ref="createInput" />  {/*ref is an identifier state for the DOM element*/}
         <button>Create</button>
       </form>
     );
   }
+
+  handleCreate(event) {
+      event.preventDefault();
+
+      console.log(this.props.createTask);
+  }
+
 }
